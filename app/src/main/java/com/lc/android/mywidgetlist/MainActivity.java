906598223465397742041ef9648e1a4a4225f46b
BluthLee <lichaotk@outlook.com>
2018-01-31 23:39:17 +0800

    Initial commit

---
+++ app/src/main/java/com/lc/android/mywidgetlist/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lc.android.mywidgetlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
